c5bc0321a92c1e39858804113c39ebcc62a284da
Krisrp <kristoforusrp@gmail.com>
2018-02-22 02:37:18 +0700

    Add slide layout

--- mobile/src/views/OnboardTour/index.js
+++ mobile/src/views/OnboardTour/index.js
@@ -6,15 +6,28 @@ import images from '../../../assets/images'
 
 const styles = StyleSheet.create({
   wrapper: {},
-  slide1: {
+  slide: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#FFFFFF',
   },
   logo: {
-    width: 120,
-    height: 120,
+    width: 140,
+    height: 140,
+  },
+  buttonWrapper: {
+    flex: 0.8,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '100%',
+  },
+  button: {
+    flexDirection: 'row',
+    width: '80%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 4,
   },
   slide2: {
     flex: 1,
@@ -35,131 +48,190 @@ const styles = StyleSheet.create({
   },
   greyText: {
     color: '#ACACAC',
-    fontSize: 12,
+    fontSize: 14,
     fontWeight: 'bold',
   },
 })
 
-const OnboardTour = () => ({
-  render() {
-    return (
-      <Swiper style={styles.wrapper}>
-        <View style={styles.slide1}>
-          <View
+const OnboardTour = () => (
+  <Swiper style={styles.wrapper} paginationStyle={{ bottom: 40 }}>
+    <Slide1 />
+    <Slide2 />
+    <View style={styles.slide3}>
+      <Text style={styles.text}>And simple</Text>
+    </View>
+    <View style={styles.slide3}>
+      <Text style={styles.text}>And simple</Text>
+    </View>
+  </Swiper>
+)
+
+const Slide1 = () => (
+  <View style={styles.slide}>
+    <View
+      style={{
+        flex: 1.4,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <View style={{ flex: 0.45 }}>
+        <Image source={images.logo} style={styles.logo} />
+      </View>
+      <View style={{ flex: 0.1 }}>
+        <Text style={styles.mainText}>Welcome to Blue Collar Lists</Text>
+      </View>
+      <View>
+        <Text style={styles.greyText}>Swipe to learn more</Text>
+      </View>
+    </View>
+    <View style={styles.buttonWrapper}>
+      <View style={styles.buttonWrapper}>
+        <TouchableOpacity
+          style={[
+            styles.button,
+            {
+              flex: 0.4,
+              backgroundColor: '#32679A',
+            },
+          ]}
+        >
+          <Text
             style={{
-              flex: 1.4,
-              justifyContent: 'center',
-              alignItems: 'center',
+              color: '#fff',
+              textAlign: 'center',
+              fontSize: 16,
             }}
           >
-            <View style={{ flex: 0.5 }}>
-              <Image source={images.logo} style={styles.logo} />
-            </View>
-            <View style={{ flex: 0.1 }}>
-              <Text style={styles.mainText}>Welcome to Blue Collar Lists</Text>
-            </View>
-            <View>
-              <Text style={styles.greyText}>Swipe to learn more</Text>
-            </View>
+            Sign Up
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.button,
+            {
+              flex: 0.36,
+              borderWidth: 1,
+              borderColor: '#32679A',
+              backgroundColor: '#FFFFFF',
+            },
+          ]}
+        >
+          <Text
+            style={{
+              color: '#32679A',
+              textAlign: 'center',
+              fontSize: 16,
+            }}
+          >
+            Log In
+          </Text>
+        </TouchableOpacity>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <View style={{ marginRight: 8 }}>
+            <Text style={[styles.greyText, { fontSize: 10 }]}>
+              Want to see how it works?
+            </Text>
           </View>
-          <View
-            style={{
-              flex: 1,
-              justifyContent: 'space-around',
-              alignItems: 'center',
-              width: '100%',
-            }}
-          >
-            <View
-              style={{
-                flex: 1,
-                justifyContent: 'space-around',
-                alignItems: 'center',
-                width: '100%',
-              }}
+          <TouchableOpacity>
+            <Text
+              textDecorationLine="underline"
+              textDecorationColor="#32679A"
+              style={{ fontSize: 12 }}
             >
-              <TouchableOpacity
-                style={{
-                  flex: 0.3,
-                  flexDirection: 'row',
-                  width: '80%',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  backgroundColor: '#32679A',
-                  borderRadius: 4,
-                }}
-              >
-                <Text
-                  style={{
-                    color: '#fff',
-                    textAlign: 'center',
-                    fontSize: 16,
-                  }}
-                >
-                  Sign Up
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={{
-                  flex: 0.26,
-                  flexDirection: 'row',
-                  width: '80%',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  borderRadius: 4,
-                  borderWidth: 1,
-                  borderColor: '#32679A',
-                  backgroundColor: '#FFFFFF',
-                }}
-              >
-                <Text
-                  style={{
-                    color: '#32679A',
-                    textAlign: 'center',
-                    fontSize: 16,
-                  }}
-                >
-                  Log In
-                </Text>
-              </TouchableOpacity>
-              <View
-                style={{
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                  alignItems: 'center',
-                }}
-              >
-                <View style={{ marginRight: 8 }}>
-                  <Text style={[styles.greyText, { fontSize: 10 }]}>
-                    Want to see how it works?
-                  </Text>
-                </View>
-                <TouchableOpacity>
-                  <Text
-                    textDecorationLine="underline"
-                    textDecorationColor="#32679A"
-                    style={{ fontSize: 12 }}
-                  >
-                    Take the tour
-                  </Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-            <View style={{ flex: 0.4 }} />
-          </View>
+              Take the tour
+            </Text>
+          </TouchableOpacity>
         </View>
-        <View style={styles.slide2}>
-          <Text style={styles.text}>Beautiful</Text>
-        </View>
-        <View style={styles.slide3}>
-          <Text style={styles.text}>And simple</Text>
-        </View>
-        <View style={styles.slide3}>
-          <Text style={styles.text}>And simple</Text>
-        </View>
-      </Swiper>
-    )
-  },
-})
+      </View>
+      <View style={{ flex: 0.5 }} />
+    </View>
+  </View>
+)
+
+const Slide2 = () => (
+  <View style={styles.slide}>
+    <View
+      style={{
+        flex: 1.4,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <View style={{ flex: 0.65 }}>
+        <Image source={images.searchimg} style={{ width: 200, height: 200 }} />
+      </View>
+      <View style={{ flex: 0.15 }}>
+        <Text style={{ color: '#54789B', fontSize: 24 }}>
+          Save Time and Money
+        </Text>
+      </View>
+      <View
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Text style={{ fontSize: 12, textAlign: 'center' }}>
+          Search and get valuable information from
+        </Text>
+        <Text style={{ fontSize: 12, textAlign: 'center' }}>
+          contractors like you before bidding your next job
+        </Text>
+      </View>
+    </View>
+    <View style={styles.buttonWrapper}>
+      <View style={styles.buttonWrapper}>
+        <TouchableOpacity
+          style={[
+            styles.button,
+            {
+              flex: 0.4,
+              backgroundColor: '#32679A',
+            },
+          ]}
+        >
+          <Text
+            style={{
+              color: '#fff',
+              textAlign: 'center',
+              fontSize: 16,
+            }}
+          >
+            Sign Up
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.button,
+            {
+              flex: 0.36,
+              borderWidth: 1,
+              borderColor: '#32679A',
+              backgroundColor: '#FFFFFF',
+            },
+          ]}
+        >
+          <Text
+            style={{
+              color: '#32679A',
+              textAlign: 'center',
+              fontSize: 16,
+            }}
+          >
+            Log In
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={{ flex: 0.5 }} />
+    </View>
+  </View>
+)
 
 export default OnboardTour
