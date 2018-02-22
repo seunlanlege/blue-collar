def3de52ec30e437bc415a63f489c43ef42e0e44
Krisrp <kristoforusrp@gmail.com>
2018-02-22 10:30:14 +0700

    Add image bid and reward

--- mobile/assets/images/index.js
+++ mobile/assets/images/index.js
@@ -1,9 +1,13 @@
 const logo = require('./logo.png')
 const icon = require('./icon.png')
 const searchimg = require('./searchimg.png')
+const bid = require('./bid.png')
+const reward = require('./reward.png')
 
 export default {
   logo,
   icon,
   searchimg,
+  bid,
+  reward,
 }
