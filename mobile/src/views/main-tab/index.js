c5aff95f6c10531eff279d829c9bc49d034813ee
Krisrp <kristoforusrp@gmail.com>
2018-02-24 17:18:17 +0700

    Finished tab-navigation UI

--- mobile/src/views/main-tab/index.js
+++ mobile/src/views/main-tab/index.js
@@ -32,39 +32,174 @@ const MainTabNavigator = () => (
       height: TAB_HEIGHT,
     }}
   >
-    <TouchableOpacity>
-      <Image source={images.searchIcon} style={{ width: 24, height: 24 }} />
-      <Text style={{ color: '#2F669C', fontSize: 10 }}>Search</Text>
-    </TouchableOpacity>
     <TouchableOpacity
       style={{
+        width: TAB_HEIGHT,
+        height: '100%',
         alignItems: 'center',
         justifyContent: 'space-around',
       }}
     >
-      <Image source={images.inviteIcon} style={{ width: 32, height: 32 }} />
-      <Text style={{ color: '#2F669C', fontSize: 12 }}>Invite</Text>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'flex-end',
+          width: '100%',
+        }}
+      >
+        <Image
+          source={images.searchIcon}
+          style={{
+            width: 29,
+            height: 29,
+          }}
+        />
+      </View>
+      <View
+        style={{
+          flex: 0.5,
+          alignItems: 'center',
+          justifyContent: 'flex-start',
+        }}
+      >
+        <Text style={{ color: '#2F669C', fontSize: 12 }}>Search</Text>
+      </View>
     </TouchableOpacity>
     <TouchableOpacity
       style={{
+        width: TAB_HEIGHT,
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'flex-start',
+          width: '100%',
+        }}
+      >
+        <Image
+          source={images.inviteIcon}
+          style={{
+            width: 42,
+            height: 42,
+          }}
+        />
+      </View>
+      <View
+        style={{
+          flex: 0.5,
+          alignItems: 'center',
+          justifyContent: 'flex-start',
+        }}
+      >
+        <Text style={{ color: '#2F669C', fontSize: 12 }}>Invite</Text>
+      </View>
+    </TouchableOpacity>
+    <TouchableOpacity
+      style={{
+        height: '100%',
         alignItems: 'center',
         justifyContent: 'space-between',
         backgroundColor: '#2F669C',
+        paddingLeft: 2,
+        paddingRight: 2,
       }}
     >
-      <Image
-        source={images.writeReviewIcon}
-        style={{ width: 32, height: 32 }}
-      />
-      <Text style={{ color: '#FFFEFE', fontSize: 12 }}>Write Review</Text>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100%',
+        }}
+      >
+        <Image
+          source={images.writeReviewIcon}
+          style={{ width: 28, height: 28 }}
+        />
+      </View>
+      <View
+        style={{
+          flex: 0.5,
+          alignItems: 'center',
+          justifyContent: 'flex-start',
+        }}
+      >
+        <Text style={{ color: '#FFFEFE', fontSize: 12 }}>Write Review</Text>
+      </View>
     </TouchableOpacity>
-    <TouchableOpacity>
-      <Image source={images.rewardIcon} style={{ width: 32, height: 32 }} />
-      <Text style={{ color: '#2F669C', fontSize: 12 }}>Rewards</Text>
+    <TouchableOpacity
+      style={{
+        width: TAB_HEIGHT,
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'flex-end',
+          width: '100%',
+        }}
+      >
+        <Image
+          source={images.rewardIcon}
+          style={{
+            width: 34,
+            height: 30,
+          }}
+        />
+      </View>
+      <View
+        style={{
+          flex: 0.5,
+          alignItems: 'center',
+          justifyContent: 'flex-start',
+        }}
+      >
+        <Text style={{ color: '#2F669C', fontSize: 12 }}>Rewards</Text>
+      </View>
     </TouchableOpacity>
-    <TouchableOpacity>
-      <Image source={images.profileIcon} style={{ width: 32, height: 32 }} />
-      <Text style={{ color: '#2F669C', fontSize: 12 }}>Profile</Text>
+    <TouchableOpacity
+      style={{
+        width: TAB_HEIGHT,
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'flex-end',
+          width: '100%',
+        }}
+      >
+        <Image
+          source={images.profileIcon}
+          style={{
+            width: 32,
+            height: 32,
+          }}
+        />
+      </View>
+      <View
+        style={{
+          flex: 0.5,
+          alignItems: 'center',
+          justifyContent: 'flex-start',
+        }}
+      >
+        <Text style={{ color: '#2F669C', fontSize: 12 }}>Profile</Text>
+      </View>
     </TouchableOpacity>
   </View>
 )
