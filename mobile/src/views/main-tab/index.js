6a752d02a57aaaa4d8577db29b9a834992fc574f
Krisrp <kristoforusrp@gmail.com>
2018-02-24 21:04:39 +0700

    Update style

--- mobile/src/views/main-tab/index.js
+++ mobile/src/views/main-tab/index.js
@@ -1,5 +1,12 @@
 import React from 'react'
-import { Dimensions, Image, View, Text, TouchableOpacity } from 'react-native'
+import {
+  Dimensions,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native'
 import { TabNavigator } from 'react-navigation'
 import images from '../../../assets/images'
 
@@ -22,58 +29,54 @@ const window = Dimensions.get('window')
 
 const TAB_HEIGHT = window.width / 6
 
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#FFFEFE',
+    height: TAB_HEIGHT,
+  },
+  tabContainer: {
+    width: TAB_HEIGHT,
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  imgContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    width: '100%',
+  },
+  titleWrapper: {
+    flex: 0.5,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  title: {
+    color: '#2F669C',
+    fontSize: 12,
+  },
+})
+
 const MainTabNavigator = () => (
-  <View
-    style={{
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      alignItems: 'center',
-      backgroundColor: '#FFFEFE',
-      height: TAB_HEIGHT,
-    }}
-  >
-    <TouchableOpacity
-      style={{
-        width: TAB_HEIGHT,
-        height: '100%',
-        alignItems: 'center',
-        justifyContent: 'space-around',
-      }}
-    >
-      <View
-        style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'flex-end',
-          width: '100%',
-        }}
-      >
+  <View style={styles.container}>
+    <TouchableOpacity style={styles.tabContainer}>
+      <View style={styles.imgContainer}>
         <Image
           source={images.searchIcon}
           style={{
-            width: 29,
-            height: 29,
+            width: 26,
+            height: 26,
           }}
         />
       </View>
-      <View
-        style={{
-          flex: 0.5,
-          alignItems: 'center',
-          justifyContent: 'flex-start',
-        }}
-      >
-        <Text style={{ color: '#2F669C', fontSize: 12 }}>Search</Text>
+      <View style={styles.titleWrapper}>
+        <Text style={styles.title}>Search</Text>
       </View>
     </TouchableOpacity>
-    <TouchableOpacity
-      style={{
-        width: TAB_HEIGHT,
-        height: '100%',
-        alignItems: 'center',
-        justifyContent: 'space-around',
-      }}
-    >
+    <TouchableOpacity style={styles.tabContainer}>
       <View
         style={{
           flex: 1,
@@ -90,14 +93,8 @@ const MainTabNavigator = () => (
           }}
         />
       </View>
-      <View
-        style={{
-          flex: 0.5,
-          alignItems: 'center',
-          justifyContent: 'flex-start',
-        }}
-      >
-        <Text style={{ color: '#2F669C', fontSize: 12 }}>Invite</Text>
+      <View style={styles.titleWrapper}>
+        <Text style={styles.title}>Invite</Text>
       </View>
     </TouchableOpacity>
     <TouchableOpacity
@@ -123,66 +120,26 @@ const MainTabNavigator = () => (
           style={{ width: 28, height: 28 }}
         />
       </View>
-      <View
-        style={{
-          flex: 0.5,
-          alignItems: 'center',
-          justifyContent: 'flex-start',
-        }}
-      >
+      <View style={styles.titleWrapper}>
         <Text style={{ color: '#FFFEFE', fontSize: 12 }}>Write Review</Text>
       </View>
     </TouchableOpacity>
-    <TouchableOpacity
-      style={{
-        width: TAB_HEIGHT,
-        height: '100%',
-        alignItems: 'center',
-        justifyContent: 'space-around',
-      }}
-    >
-      <View
-        style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'flex-end',
-          width: '100%',
-        }}
-      >
+    <TouchableOpacity style={styles.tabContainer}>
+      <View style={styles.imgContainer}>
         <Image
           source={images.rewardIcon}
           style={{
             width: 34,
-            height: 30,
+            height: 32,
           }}
         />
       </View>
-      <View
-        style={{
-          flex: 0.5,
-          alignItems: 'center',
-          justifyContent: 'flex-start',
-        }}
-      >
-        <Text style={{ color: '#2F669C', fontSize: 12 }}>Rewards</Text>
+      <View style={styles.titleWrapper}>
+        <Text style={styles.title}>Rewards</Text>
       </View>
     </TouchableOpacity>
-    <TouchableOpacity
-      style={{
-        width: TAB_HEIGHT,
-        height: '100%',
-        alignItems: 'center',
-        justifyContent: 'space-around',
-      }}
-    >
-      <View
-        style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'flex-end',
-          width: '100%',
-        }}
-      >
+    <TouchableOpacity style={styles.tabContainer}>
+      <View style={styles.imgContainer}>
         <Image
           source={images.profileIcon}
           style={{
@@ -191,14 +148,8 @@ const MainTabNavigator = () => (
           }}
         />
       </View>
-      <View
-        style={{
-          flex: 0.5,
-          alignItems: 'center',
-          justifyContent: 'flex-start',
-        }}
-      >
-        <Text style={{ color: '#2F669C', fontSize: 12 }}>Profile</Text>
+      <View style={styles.titleWrapper}>
+        <Text style={styles.title}>Profile</Text>
       </View>
     </TouchableOpacity>
   </View>
