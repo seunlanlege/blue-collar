47edfa8d262f935b463fcc0b9a64d3394cc7543f
Krisrp <kristoforusrp@gmail.com>
2018-02-26 07:18:29 +0700

    Move epic

--- mobile/src/redux/epics/index.js
+++ mobile/src/redux/epics/index.js
@@ -1,4 +1,4 @@
 import { combineEpics } from 'redux-observable'
-import { fetchReviewEpic } from '../effects/reviews-fx'
+import { fetchReviewEpic } from './review-epic'
 
 export default combineEpics(fetchReviewEpic)
