1c69f09104b10134d88e3fcb914f791c25e31c8c
Krisrp <kristoforusrp@gmail.com>
2018-02-28 01:40:34 +0700

    Add assets

--- mobile/assets/images/index.js
+++ mobile/assets/images/index.js
@@ -1,6 +1,8 @@
 const bid = require('./bid.png')
+const companyIcon = require('./icon-company.png')
 const email = require('./email.png')
 const inviteIcon = require('./icon-invite.png')
+const locationIcon = require('./icon-location.png')
 const logo = require('./logo.png')
 const password = require('./password.png')
 const profileIcon = require('./icon-profile.png')
@@ -9,13 +11,19 @@ const rewardIcon = require('./icon-reward.png')
 const searchIcon = require('./icon-search.png')
 const searchImg = require('./search-img.png')
 const searchTextInput = require('./icon-search-textinput.png')
+const smallLogo = require('./small-logo.png')
+const tradeIcon = require('./icon-trade.png')
 const tradePlumberIcon = require('./256-trade-plumber.png')
+const triangleIcon = require('./icon-triangle.png')
+const userIcon = require('./icon-user.png')
 const writeReviewIcon = require('./icon-write-review.png')
 
 export default {
   bid,
+  companyIcon,
   email,
   inviteIcon,
+  locationIcon,
   logo,
   password,
   profileIcon,
@@ -24,6 +32,10 @@ export default {
   searchIcon,
   searchImg,
   searchTextInput,
+  smallLogo,
+  tradeIcon,
   tradePlumberIcon,
+  triangleIcon,
+  userIcon,
   writeReviewIcon,
 }
