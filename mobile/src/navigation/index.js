1f911d38f551667be5a8806564ed0753c07db437
Krisrp <kristoforusrp@gmail.com>
2018-02-27 20:38:49 +0700

    Bind navigation and views

---
+++ mobile/src/navigation/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {
+  createReduxBoundAddListener,
+  createReactNavigationReduxMiddleware,
+} from 'react-navigation-redux-helpers'
+import { addNavigationHelpers } from 'react-navigation'
+import { connect } from 'react-redux'
+import AppNavigator from '../views'
+
+export const navigationMiddleware = createReactNavigationReduxMiddleware(
+  'root',
+  state => state.navigation,
+)
+
+const addListener = createReduxBoundAddListener('root')
+
+const NavigationWithRedux = ({ navigation, dispatch }) => (
+  <AppNavigator
+    navigation={addNavigationHelpers({
+      dispatch,
+      state: navigation,
+      addListener,
+    })}
+  />
+)
+
+const mapStateToProps = state => ({
+  navigation: state.navigation,
+})
+
+export default connect(mapStateToProps)(NavigationWithRedux)
