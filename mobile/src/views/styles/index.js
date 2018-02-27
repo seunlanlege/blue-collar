e0139fa2424043238ed33f47fec6d0d03cc11f08
Krisrp <kristoforusrp@gmail.com>
2018-02-28 01:41:46 +0700

    Add global style

---
+++ mobile/src/views/styles/index.js
@@ -0,0 +1,46 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    top: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+  },
+  textInputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  textInputInner: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  textInputIcon: {
+    width: 40,
+    height: 40,
+    borderRightWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#CCCCCC',
+  },
+  textInput: {
+    height: 40,
+    paddingLeft: 12,
+    borderWidth: 1,
+    borderLeftWidth: 0,
+    borderColor: '#CCCCCC',
+  },
+  signUpButton: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFFFFF',
+    borderWidth: 1,
+    borderColor: '#4369B0',
+    borderRadius: 6,
+  },
+})
+
+export default styles
