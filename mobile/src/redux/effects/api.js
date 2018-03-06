b55bb233775386b6a90d35c31ec284a398bf33d1
Krisrp <kristoforusrp@gmail.com>
2018-03-06 20:58:26 +0700

    Update api for search review

--- mobile/src/redux/effects/api.js
+++ mobile/src/redux/effects/api.js
@@ -4,3 +4,8 @@ export const fetchReview = () =>
   axios
     .get('https://jsonplaceholder.typicode.com/posts/1')
     .then(response => response.data)
+
+export const searchReview = query =>
+  axios
+    .get('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.data)
