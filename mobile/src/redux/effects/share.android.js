d368c49190fe5330ee1bbc983cb3c448138b841a
Krisrp <kristoforusrp@gmail.com>
2018-03-17 03:06:30 +0700

    Initial share effect and epic

---
+++ mobile/src/redux/effects/share.android.js
@@ -0,0 +1,7 @@
+import { Share } from 'react-native'
+
+export const shareInviteCode = () =>
+  Share.share({
+    message:
+      'Griff has invited you to try a FREE month trial of Blue Collar Lists. To claim your free gift, sign up using this link: https://www.bluecollarlists/shftjvf',
+  })
