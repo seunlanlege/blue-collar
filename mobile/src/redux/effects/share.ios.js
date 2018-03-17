86f8ecaf6ea4b91c71693a837a64f56edd9db7e4
Krisrp <kristoforusrp@gmail.com>
2018-03-17 10:11:34 +0700

    add share effects

--- mobile/src/redux/effects/share.ios.js
+++ mobile/src/redux/effects/share.ios.js
@@ -1,6 +1,6 @@
 import { Share } from 'react-native'
 
-export const shareInviteCode = () =>
+export const shareApp = () =>
   Share.share({
     url:
       'Griff has invited you to try a FREE month trial of Blue Collar Lists. To claim your free gift, sign up using this link: https://www.bluecollarlists/shftjvf',
