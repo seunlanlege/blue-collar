29ac2c99ec07673184d9a6fa24fd2c3e5522e8be
Krisrp <kristoforusrp@gmail.com>
2018-03-17 10:12:05 +0700

    update effects

--- mobile/src/redux/epics/index.js
+++ mobile/src/redux/epics/index.js
@@ -3,12 +3,14 @@ import { fetchReviewEpic, searchReviewEpic } from './review-epic'
 // import { signUpRequest } from './signup-epic'
 import { fetchRewardEpic, redeemPointEpic } from './reward-epic'
 import { logInRequestEpic, logOutRequestEpic } from './login'
+import { shareAppEpic } from './share'
 
 export default combineEpics(
   fetchReviewEpic,
   fetchRewardEpic,
-  redeemPointEpic,
-  searchReviewEpic,
   logInRequestEpic,
   logOutRequestEpic,
+  redeemPointEpic,
+  searchReviewEpic,
+  shareAppEpic,
 )
---
+++ mobile/src/redux/epics/share.js
@@ -0,0 +1,12 @@
+import { Observable } from 'rxjs'
+import { shareActions, SHARE_ACTIONS } from '../modules/share'
+/* eslint-disable */
+import { shareApp } from '../effects/share'
+/* eslint-enable */
+
+export const shareAppEpic = action$ =>
+  action$.ofType(SHARE_ACTIONS.REQUEST).switchMap(action =>
+    Observable.of(shareApp())
+      .map(shareActions.fulfilled)
+      .catch(error => Observable.of(shareActions.rejected(error))),
+  )
