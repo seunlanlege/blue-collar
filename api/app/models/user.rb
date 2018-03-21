64d3a275fd523b217d86df5327a5cd16fb4eb3b4
Krisrp <kristoforusrp@gmail.com>
2018-03-21 17:07:51 +0700

    Add venues

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :venue_reviews, dependent: :destroy
   include DeviseTokenAuth::Concerns::User
 end
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321015834) do
+ActiveRecord::Schema.define(version: 20180321092441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,35 @@ ActiveRecord::Schema.define(version: 20180321015834) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+  end
+
+  create_table "venue_reviews", force: :cascade do |t|
+    t.integer "venue_id"
+    t.integer "reviewer_id"
+    t.integer "point_of_contact_type", default: 0
+    t.text "comments"
+    t.integer "star_bid_process"
+    t.integer "star_change_orders_accepted"
+    t.integer "star_time_respected"
+    t.integer "star_job_completed"
+    t.integer "star_payments_satifaction"
+    t.integer "star_work_with_again"
+    t.float "star_overall"
+    t.boolean "bought"
+    t.boolean "other_party_involved"
+    t.decimal "dollars_lost", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["reviewer_id"], name: "index_venue_reviews_on_reviewer_id"
+    t.index ["venue_id"], name: "index_venue_reviews_on_venue_id"
+  end
+
+  create_table "venues", force: :cascade do |t|
+    t.string "place_id"
+    t.string "name"
+    t.string "vicinity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
