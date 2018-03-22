afeaee39079cb610660d77bd9f4a333e7d0946ec
Krisrp <kristoforusrp@gmail.com>
2018-03-23 03:34:35 +0700

    Update model

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -3,6 +3,27 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_many :venue_reviews, dependent: :destroy
+  has_and_belongs_to_many :companies, through: :companies_users
+
+  enum trade: {
+      carpenter: 0,
+      cleanouts_demolition: 1,
+      electrician: 2,
+      general_contractor: 3,
+      gutter: 4,
+      fence: 5, framer: 6,
+      flooring_tile: 7,
+      hvac: 8,
+      landscaping: 9,
+      mason: 10,
+      movers: 11,
+      plasterer_or_drywall: 12,
+      plumber: 13,
+      painter: 14,
+      roofer: 15,
+      tree_services: 16,
+      other: 17 }
   include DeviseTokenAuth::Concerns::User
 end
