740466bdfbd3387407011c68e92b05723895f645
Krisrp <kristoforusrp@gmail.com>
2018-03-24 11:29:44 +0700

    Update url

--- mobile/config.js
+++ mobile/config.js
@@ -1,10 +1,12 @@
 const config = Object.freeze({
   APP_NAME: 'blue-collar',
   FACEBOOK_APP_ID: '337883893384348',
+  GOOLE_PLACE_API: 'AIzaSyCo8D74SL6feiIPwn3Y5xVssMKWIthQjjs',
+  GOOGLE_PLACE_URL: `https://maps.googleapis.com/maps/api/place/nearbysearch`,
   API_BASE_URL: 'http://localhost:3001',
   SIGN_UP_URL: '/auth',
   LOG_IN_URL: '/auth/sign_in',
-  GOOLE_PLACE_API: 'AIzaSyCo8D74SL6feiIPwn3Y5xVssMKWIthQjjs',
+  VENUE_URL: '/venues',
 })
 
 export default config
