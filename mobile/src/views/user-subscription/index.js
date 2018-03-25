8e8c343b6517d8915be6ab5e7b175d8820ebbee4
Krisrp <kristoforusrp@gmail.com>
2018-03-26 01:20:40 +0700

    Move style to variable

--- mobile/src/views/user-subscription/index.js
+++ mobile/src/views/user-subscription/index.js
@@ -3,6 +3,7 @@ import {
   Image,
   Dimensions,
   KeyboardAvoidingView,
+  StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
@@ -17,6 +18,78 @@ import images from '../../../assets/images'
 import styles from '../shared/styles'
 
 const IMAGE_HEIGHT = Dimensions.get('window').width / 1.8
+
+const localStyles = StyleSheet.create({
+  container: {
+    flex: 0.6,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  creditCard: {
+    width: IMAGE_HEIGHT,
+    height: IMAGE_HEIGHT,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  promo: {
+    fontSize: 16,
+    color: '#2F669C',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  keyboardAvoidingView: {
+    flex: 0.5,
+    width: '80%',
+    justifyContent: 'space-around',
+  },
+  smallTextInputWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderRadius: 2,
+  },
+  textInput: {
+    fontSize: 11,
+    height: 45,
+    textAlign: 'center',
+    borderRadius: 2,
+    borderLeftWidth: 1,
+  },
+  smallTextInput: {
+    fontSize: 11,
+    height: 45,
+    textAlign: 'center',
+    borderLeftWidth: 1,
+    paddingLeft: 0,
+  },
+  footerWrapper: {
+    flex: 0.3,
+    width: '80%',
+    justifyContent: 'flex-start',
+  },
+  innerWrapper: {
+    flex: 0.7,
+    justifyContent: 'space-around',
+  },
+  promoCode: {
+    textAlign: 'center',
+    textDecorationLine: 'underline',
+    textDecorationStyle: 'solid',
+    textDecorationColor: '#CCCCCC',
+    color: '#CCCCCC',
+  },
+  buttonWrapper: {
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#2F669C',
+    borderColor: '#4369B0',
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 20,
+  },
+})
 
 const mapStateToProps = state => state.userSubscription
 
@@ -48,98 +121,41 @@ class UserSubscription extends React.Component {
 
     return (
       <View style={styles.container}>
-        <View
-          style={{
-            flex: 0.6,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
+        <View style={localStyles.container}>
           <View>
-            <Image
-              source={images.creditCard}
-              style={{
-                width: IMAGE_HEIGHT,
-                height: IMAGE_HEIGHT,
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-            />
+            <Image source={images.creditCard} style={localStyles.creditCard} />
           </View>
           <View style={{ width: '95%' }}>
-            <Text
-              style={{ fontSize: 18, color: '#2F669C', textAlign: 'center' }}
-            >
+            <Text style={localStyles.promo}>
               TRY FREE for 30 days! membership only $24.99/mo After trial.{' '}
             </Text>
-            <Text
-              style={{ fontSize: 18, color: '#2F669C', textAlign: 'center' }}
-            >
-              Cancel anytime.
-            </Text>
+            <Text style={localStyles.promo}>Cancel anytime.</Text>
           </View>
         </View>
 
         <KeyboardAvoidingView
           behavior="padding"
-          style={{
-            flex: 0.5,
-            width: '80%',
-            justifyContent: 'space-around',
-          }}
+          style={localStyles.keyboardAvoidingView}
         >
           <View>
             <TextInput
               placeholder="Card Number"
-              style={[
-                styles.textInput,
-                {
-                  fontSize: 11,
-                  height: 45,
-                  textAlign: 'center',
-                  borderRadius: 2,
-                  borderLeftWidth: 1,
-                },
-              ]}
+              style={[styles.textInput, localStyles.textInput]}
               onChangeText={text => updateFieldFn('cardNumber', text)}
               value={cardNumber}
             />
           </View>
           <TextInput
             placeholder="Cardholder Name"
-            style={[
-              styles.textInput,
-              {
-                fontSize: 11,
-                height: 45,
-                textAlign: 'center',
-                borderRadius: 2,
-                borderLeftWidth: 1,
-              },
-            ]}
+            style={[styles.textInput, localStyles.textInput]}
             onChangeText={text => updateFieldFn('cardHolderName', text)}
             value={cardHolderName}
           />
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              borderRadius: 2,
-            }}
-          >
+          <View style={localStyles.smallTextInputWrapper}>
             <View style={{ width: '45%' }}>
               <TextInput
                 placeholder="Expiration Date"
-                style={[
-                  styles.textInput,
-                  {
-                    fontSize: 11,
-                    height: 45,
-                    textAlign: 'center',
-                    borderLeftWidth: 1,
-                    paddingLeft: 0,
-                  },
-                ]}
+                style={[styles.textInput, localStyles.smallTextInput]}
                 onChangeText={text => updateFieldFn('expirationDate', text)}
                 value={expirationDate}
               />
@@ -147,16 +163,7 @@ class UserSubscription extends React.Component {
             <View style={{ width: '45%' }}>
               <TextInput
                 placeholder="CVV"
-                style={[
-                  styles.textInput,
-                  {
-                    fontSize: 11,
-                    height: 45,
-                    textAlign: 'center',
-                    borderLeftWidth: 1,
-                    paddingLeft: 0,
-                  },
-                ]}
+                style={[styles.textInput, localStyles.smallTextInput]}
                 secureTextEntry
                 onChangeText={text => updateFieldFn('cvv', text)}
                 value={cvv}
@@ -165,43 +172,15 @@ class UserSubscription extends React.Component {
           </View>
         </KeyboardAvoidingView>
 
-        <View
-          style={{
-            flex: 0.3,
-            width: '80%',
-            justifyContent: 'flex-start',
-          }}
-        >
-          <View
-            style={{
-              flex: 0.7,
-              justifyContent: 'space-around',
-            }}
-          >
+        <View style={localStyles.footerWrapper}>
+          <View style={localStyles.innerWrapper}>
             <TouchableOpacity stle={{ flex: 0 }}>
-              <Text
-                style={{
-                  textAlign: 'center',
-                  textDecorationLine: 'underline',
-                  textDecorationStyle: 'solid',
-                  textDecorationColor: '#CCCCCC',
-                  color: '#CCCCCC',
-                }}
-              >
-                Have a promo code?
-              </Text>
+              <Text style={localStyles.promoCode}>Have a promo code?</Text>
             </TouchableOpacity>
 
             <TouchableOpacity
               onPress={() => reqSubscriptionFn()}
-              style={{
-                height: 45,
-                alignItems: 'center',
-                justifyContent: 'center',
-                backgroundColor: '#2F669C',
-                borderColor: '#4369B0',
-                borderRadius: 6,
-              }}
+              style={localStyles.buttonWrapper}
               disabled={
                 cardNumber.length < 3 ||
                 cardHolderName.length < 4 ||
@@ -209,9 +188,7 @@ class UserSubscription extends React.Component {
                 expirationDate.email < 3
               }
             >
-              <Text style={{ color: '#FFFFFF', fontSize: 20 }}>
-                Start Your Free Trial
-              </Text>
+              <Text style={localStyles.buttonText}>Start Your Free Trial</Text>
             </TouchableOpacity>
           </View>
         </View>
