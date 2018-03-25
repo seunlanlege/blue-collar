f781169209650f39b73df57f4c05b455c53b001b
Krisrp <kristoforusrp@gmail.com>
2018-03-25 20:35:50 +0700

    Add helper

---
+++ mobile/src/redux/effects/api/utils.js
@@ -0,0 +1,37 @@
+export const authHeader = ({
+  accessToken,
+  tokenType,
+  client,
+  expiry,
+  uid,
+}) => ({
+  headers: {
+    'access-token': accessToken,
+    'token-type': tokenType,
+    client,
+    expiry,
+    uid,
+  },
+})
+
+export const setUserData = ({
+  firstName,
+  lastName,
+  trade,
+  jobPosition,
+  vicinity,
+  placeId,
+  name,
+}) => ({
+  user: {
+    first_name: firstName,
+    last_name: lastName,
+    trade: trade.toLowerCase().replace(/\s/g, '_'),
+    job_position: jobPosition,
+    company: {
+      vicinity,
+      place_id: placeId,
+      name,
+    },
+  },
+})
