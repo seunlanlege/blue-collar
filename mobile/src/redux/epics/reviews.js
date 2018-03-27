059e961be3e6ab9e2bde765255c3604cf2d39214
Krisrp <kristoforusrp@gmail.com>
2018-03-27 10:25:28 +0700

    Rename file

---
+++ mobile/src/redux/epics/reviews.js
@@ -0,0 +1,20 @@
+import { Observable } from 'rxjs'
+
+import { reviewActions, REVIEW_ACTIONS } from '../modules/reviews'
+import { fetchReview, searchReview } from '../effects/api'
+
+export const fetchReviewEpic = action$ =>
+  action$.ofType(REVIEW_ACTIONS.FETCH).switchMap(action =>
+    Observable.fromPromise(fetchReview())
+      .map(reviewActions.fulfilled)
+      .catch(error => Observable.of(reviewActions.rejected(error.message))),
+  )
+
+export const searchReviewEpic = action$ =>
+  action$.ofType(REVIEW_ACTIONS.SEARCH).switchMap(action =>
+    Observable.fromPromise(searchReview())
+      .map(reviewActions.searchFulfilled)
+      .catch(error =>
+        Observable.of(reviewActions.fetchReviewRejected(error.message)),
+      ),
+  )
