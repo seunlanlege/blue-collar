fc24c0a312444a7c1b339d221e997ff141479a0f
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-04-05 20:40:52 -0400

    Adds index and foreign key constraints on foreign keys

--- api/db/migrate/20180322174756_add_data_to_users.rb
+++ api/db/migrate/20180322174756_add_data_to_users.rb
@@ -5,7 +5,8 @@ class AddDataToUsers < ActiveRecord::Migration[5.1]
     add_column :users, :trade, :integer
     add_column :users, :strip_token, :string
     add_column :users, :contactable, :boolean
-    add_column :users, :place_id, :integer
     add_column :users, :job_position, :string
+
+    add_reference :users, :place, index: true
   end
 end
--- api/db/migrate/20180326154758_create_place_bids.rb
+++ api/db/migrate/20180326154758_create_place_bids.rb
@@ -1,9 +1,11 @@
 class CreatePlaceBids < ActiveRecord::Migration[5.1]
   def change
     create_table :place_bids do |t|
-      t.integer :place_id
       t.integer :user_id
       t.integer :bid
+
+      t.belongs_to :place, index: true
+      t.belongs_to :user, index: true
 
       t.timestamps
     end
