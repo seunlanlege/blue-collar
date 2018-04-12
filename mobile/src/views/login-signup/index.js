b810693d325c9ee6116f3650bd15da8907752e67
Krisrp <kristoforusrp@gmail.com>
2018-04-12 08:09:33 +0700

    Add keyboard-aware-scrollview and update component style

--- mobile/src/views/login-signup/index.js
+++ mobile/src/views/login-signup/index.js
@@ -1,28 +1,57 @@
 import React from 'react'
 import {
   ActivityIndicator,
-  Animated,
-  Easing,
   Image,
   Dimensions,
-  Keyboard,
-  KeyboardAvoidingView,
   StyleSheet,
   TouchableOpacity,
   Text,
   TextInput,
   View,
 } from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scrollview'
 
 import images from '../../../assets/images'
 
+const IMAGE_HEIGHT = Dimensions.get('window').width / 3
+
 const styles = StyleSheet.create({
+  keyboardWrapper: {
+    flex: 1,
+    top: 20,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFFFFF',
+  },
+  takeTheTourWrapper: {
+    marginTop: 12,
+    width: '95%',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
   takeTheTour: {
     textDecorationLine: 'underline',
     textDecorationStyle: 'solid',
     textDecorationColor: '#3d6587',
     color: '#3d6587',
     fontWeight: 'bold',
+  },
+  imageWrapper: {
+    marginTop: 36,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: IMAGE_HEIGHT,
+    height: IMAGE_HEIGHT,
+  },
+  buttonWrapper: {
+    marginTop: 36,
+    width: '80%',
+    justifyContent: 'space-around',
   },
   signUpFacebook: {
     flex: 0.5,
@@ -33,9 +62,35 @@ const styles = StyleSheet.create({
     backgroundColor: '#495991',
     borderRadius: 6,
   },
+  facebookAuth: {
+    paddingLeft: 15,
+    color: '#FFFFFF',
+    fontWeight: '500',
+  },
+  divider: {
+    flexDirection: 'row',
+    marginTop: 20,
+  },
+  line: {
+    flex: 1,
+    borderTopWidth: 2,
+    borderColor: '#F2F2F4',
+  },
+  lineText: {
+    flex: 0.4,
+    top: -7,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputWrapper: {
+    flex: 1.5,
+    width: '80%',
+    justifyContent: 'space-between',
+  },
   textInputContainer: {
     flex: 1,
     flexDirection: 'row',
+    marginTop: 8,
   },
   textInputInner: {
     flex: 1,
@@ -55,8 +110,17 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#CCCCCC',
   },
+  bottomFooter: {
+    flex: 1.2,
+    marginTop: 20,
+    width: '80%',
+    justifyContent: 'flex-start',
+  },
   signUpButton: {
     flex: 0.4,
+    height: 45,
+    marginTop: 10,
+    marginBottom: 60,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#FFFFFF',
@@ -64,51 +128,13 @@ const styles = StyleSheet.create({
     borderColor: '#4369B0',
     borderRadius: 6,
   },
+  signUpButtonText: {
+    color: '#4369B0',
+    fontWeight: '500',
+  },
 })
 
-const window = Dimensions.get('window')
-
-const IMAGE_HEIGHT = window.width / 2.5
-const IMAGE_HEIGHT_SMALL = window.width / 4
-
 class Wrapper extends React.Component {
-  constructor(props) {
-    super(props)
-    this.imageHeight = new Animated.Value(IMAGE_HEIGHT)
-  }
-
-  componentWillMount() {
-    this.keyboardWillShowListener = Keyboard.addListener(
-      'keyboardWillShow',
-      this.keyboardWillShow,
-    )
-    this.keyboardWillHideListener = Keyboard.addListener(
-      'keyboardWillHide',
-      this.keyboardWillHide,
-    )
-  }
-
-  componentWillUnmount() {
-    this.keyboardWillShowListener.remove()
-    this.keyboardWillHideListener.remove()
-  }
-
-  keyboardWillShow = event => {
-    Animated.timing(this.imageHeight, {
-      duration: event.duration,
-      toValue: IMAGE_HEIGHT_SMALL,
-      easing: Easing.ease,
-    }).start()
-  }
-
-  keyboardWillHide = event => {
-    Animated.timing(this.imageHeight, {
-      duration: event.duration,
-      toValue: IMAGE_HEIGHT,
-      easing: Easing.ease,
-    }).start()
-  }
-  // @TODO Change dispatch(navigateAction) to onPress later, this for trigger login action
   render() {
     const {
       mainButtonTitle,
@@ -123,142 +149,91 @@ class Wrapper extends React.Component {
       facebookAuth,
     } = this.props
     return (
-      <View
-        style={{
-          flex: 1,
-          top: 20,
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: '#FFFFFF',
-        }}
+      <KeyboardAwareScrollView
+        style={styles.keyboardWrapper}
+        getTextInputRefs={() => [this.textInputRef]}
       >
-        <View
-          style={{
-            flex: 0.2,
-            width: '95%',
-            alignItems: 'flex-start',
-            justifyContent: 'center',
-          }}
-        >
-          <TouchableOpacity onPress={() => navigate('onBoard')}>
-            <Text style={styles.takeTheTour}>Take The Tour</Text>
-          </TouchableOpacity>
-        </View>
-        <View
-          style={{
-            flex: 2,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Animated.Image
-            source={images.logo}
-            style={{ width: this.imageHeight, height: this.imageHeight }}
-          />
-        </View>
-        <View
-          style={{
-            flex: 1,
-            width: '80%',
-            justifyContent: 'space-around',
-          }}
-        >
-          <TouchableOpacity
-            style={styles.signUpFacebook}
-            onPress={() => facebookAuth()}
-          >
-            <Image source={images.facebookIcon} />
-            <View>
-              <Text
-                style={{
-                  paddingLeft: 15,
-                  color: '#FFFFFF',
-                  fontWeight: '500',
-                }}
-              >
-                {mainButtonTitle}
-              </Text>
-            </View>
-          </TouchableOpacity>
-          <View style={{ flexDirection: 'row' }}>
-            <View
-              style={{ flex: 1, borderTopWidth: 1, borderColor: '#F2F2F4' }}
-            />
-            <View
-              style={{
-                flex: 0.4,
-                alignItems: 'center',
-                justifyContent: 'center',
-                top: -7,
-              }}
-            >
-              <Text style={{ fontSize: 12, color: '#D1D2D4' }}>OR</Text>
-            </View>
-            <View
-              style={{ flex: 1, borderTopWidth: 1, borderColor: '#F2F2F4' }}
-            />
-          </View>
-        </View>
-        <KeyboardAvoidingView
-          behavior="padding"
-          style={{
-            flex: 1.5,
-            width: '80%',
-            justifyContent: 'space-between',
-          }}
-        >
-          <View style={styles.textInputContainer}>
-            <Image source={images.email} style={styles.textInputIcon} />
-            <View style={styles.textInputInner}>
-              <TextInput
-                placeholder="Email Address"
-                autoCapitalize="none"
-                underlineColorAndroid="transparent"
-                autoCorrect={false}
-                style={styles.textInput}
-                value={inputField.email}
-                onChangeText={text => updateFieldFn('email', text)}
-              />
-            </View>
-          </View>
-          <View style={styles.textInputContainer}>
-            <Image source={images.password} style={styles.textInputIcon} />
-            <View style={styles.textInputInner}>
-              <TextInput
-                placeholder="Password"
-                secureTextEntry
-                underlineColorAndroid="transparent"
-                autoCorrect={false}
-                style={styles.textInput}
-                value={inputField.password}
-                onChangeText={text => updateFieldFn('password', text)}
-              />
-            </View>
-          </View>
-          <View style={{ flex: 0.8 }}>{children}</View>
-        </KeyboardAvoidingView>
-        <View
-          style={{
-            flex: 1.2,
-            width: '80%',
-            justifyContent: 'flex-start',
-          }}
-        >
-          {loading ? (
-            <ActivityIndicator color="blue" size="large" />
-          ) : (
-            <TouchableOpacity
-              style={styles.signUpButton}
-              onPress={() => onPress(authUrl, inputField)}
-              disabled={inputField.password.length < 3 || inputField.email < 3}
-            >
-              <Text style={{ color: '#4369B0', fontWeight: '500' }}>
-                {minorButtonTitle}
-              </Text>
+        <View style={styles.container}>
+          <View style={styles.takeTheTourWrapper}>
+            <TouchableOpacity onPress={() => navigate('onBoard')}>
+              <Text style={styles.takeTheTour}>Take The Tour</Text>
             </TouchableOpacity>
-          )}
+          </View>
+          <View style={styles.imageWrapper}>
+            <Image source={images.logo} style={styles.logo} />
+          </View>
+          <View style={styles.buttonWrapper}>
+            <TouchableOpacity
+              style={styles.signUpFacebook}
+              onPress={() => facebookAuth()}
+            >
+              <Image source={images.facebookIcon} />
+              <View>
+                <Text style={styles.facebookAuth}>{mainButtonTitle}</Text>
+              </View>
+            </TouchableOpacity>
+            <View style={styles.divider}>
+              <View style={styles.line} />
+              <View style={styles.lineText}>
+                <Text style={{ fontSize: 12, color: '#D1D2D4' }}>OR</Text>
+              </View>
+              <View style={styles.line} />
+            </View>
+          </View>
+          <View style={styles.inputWrapper}>
+            <View style={styles.textInputContainer}>
+              <Image source={images.email} style={styles.textInputIcon} />
+              <View style={styles.textInputInner}>
+                <TextInput
+                  placeholder="Email Address"
+                  autoCapitalize="none"
+                  underlineColorAndroid="transparent"
+                  autoCorrect={false}
+                  style={styles.textInput}
+                  value={inputField.email}
+                  onChangeText={text => updateFieldFn('email', text)}
+                  ref={r => {
+                    this.textInputRef = r
+                  }}
+                />
+              </View>
+            </View>
+            <View style={styles.textInputContainer}>
+              <Image source={images.password} style={styles.textInputIcon} />
+              <View style={styles.textInputInner}>
+                <TextInput
+                  placeholder="Password"
+                  secureTextEntry
+                  underlineColorAndroid="transparent"
+                  autoCorrect={false}
+                  style={styles.textInput}
+                  value={inputField.password}
+                  onChangeText={text => updateFieldFn('password', text)}
+                  ref={r => {
+                    this.textInputRef = r
+                  }}
+                />
+              </View>
+            </View>
+            <View style={{ flex: 0.8, marginTop: 10 }}>{children}</View>
+          </View>
+          <View style={styles.bottomFooter}>
+            {loading ? (
+              <ActivityIndicator color="blue" size="large" />
+            ) : (
+              <TouchableOpacity
+                style={styles.signUpButton}
+                onPress={() => onPress(authUrl, inputField)}
+                disabled={
+                  inputField.password.length < 3 || inputField.email < 3
+                }
+              >
+                <Text style={styles.signUpButtonText}>{minorButtonTitle}</Text>
+              </TouchableOpacity>
+            )}
+          </View>
         </View>
-      </View>
+      </KeyboardAwareScrollView>
     )
   }
 }
