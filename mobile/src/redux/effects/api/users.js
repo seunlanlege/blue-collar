c5f53b8c5d5144fc68b1957830113cf02b5e02e7
Krisrp <kristoforusrp@gmail.com>
2018-04-17 07:19:41 +0700

    Update effects users and subscription

--- mobile/src/redux/effects/api/users.js
+++ mobile/src/redux/effects/api/users.js
@@ -99,4 +99,4 @@ export const update = ({
       },
       place: adaptPlaceParams(place),
     },
-  }).then(({ data }) => ({ user: parseUser(data) }))
+  }).then(({ data }) => ({ user: parseUser(data), authHeaders }))
