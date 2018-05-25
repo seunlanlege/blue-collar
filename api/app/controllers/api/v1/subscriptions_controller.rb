e00ffc8fefa9741e9b229cbbd9dc0e82e585e6f8
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-05-25 18:56:50 -0400

    fix stripe error

--- api/app/controllers/api/v1/subscriptions_controller.rb
+++ api/app/controllers/api/v1/subscriptions_controller.rb
@@ -15,14 +15,14 @@ module Api
         stripe_subscription = Stripe::Subscription.create(
           customer: stripe_customer.id,
           items: [{plan: STRIPE_CONFIG[:plan_id]}],
-          trial_end: 1.month.from_now.to_i
+          trial_end: 1.month.from_now.to_i,
         )
 
         @subscription = Subscription.new(
           user: @user,
           stripe_customer_id: stripe_customer.id,
           stripe_subscription_id: stripe_subscription.id,
-          stripe_customer_source: stripe_customer_source,
+          stripe_customer_source: stripe_customer.source,
         )
 
         if @subscription.save
