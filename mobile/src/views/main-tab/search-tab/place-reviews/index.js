b47ee6a70b4eee8a909435909cf859a64b84e28f
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-06-05 16:21:34 -0400

    Revert "Extend bg color to be blue"
    This reverts commit d82a7076d7140e6d29a48ed419c5aec3c1a4c654.

--- mobile/src/views/main-tab/search-tab/place-reviews/index.js
+++ mobile/src/views/main-tab/search-tab/place-reviews/index.js
@@ -34,7 +34,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#2F669C',
+    backgroundColor: '#FFFFFF',
   },
   searchContainer: {
     flex: 1,
--- mobile/src/views/main-tab/search-tab/reviews/index.js
+++ mobile/src/views/main-tab/search-tab/reviews/index.js
@@ -33,7 +33,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#2F669C',
+    backgroundColor: '#FFFFFF',
   },
   searchContainer: {
     flex: 0.18,
